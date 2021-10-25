71c8f695edc9f0c07637d14f4cb3032722680a9b
Emmanuel-tuyisenge <tuyisenge.emmanuel@yahoo.fr>
2021-10-25 16:21:12 +0200

    retrieve data using DAO

--- src/com/manu/springdemo/controller/CustomerController.java
+++ src/com/manu/springdemo/controller/CustomerController.java
@@ -1,15 +1,31 @@
 package com.manu.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.manu.springdemo.dao.CustomerDAO;
+import com.manu.springdemo.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+	// need to inject the customer dao
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel) {
+		
+		// get customers from the dao
+		List<Customer> theCustomers = customerDAO.getCustomers();
+		
+		// add the customers to the model
+		theModel.addAttribute("customers", theCustomers);
 		
 		return "list-customers";
 	}
---
+++ src/com/manu/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.manu.springdemo.dao;
+
+import java.util.List;
+
+import com.manu.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+}
